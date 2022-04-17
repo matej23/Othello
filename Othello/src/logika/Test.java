c5649c1aef7e49ead476a5f146c8f0c64c7cb531
MATEJ NOVOSELEC <matej.novoselec@gmail.com>
2022-04-17 14:32:36 +0200

    Test prikazuje stanje :)  ... terminal :(

---
+++ Othello/src/logika/Test.java
@@ -0,0 +1,48 @@
+package logika;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+import splosno.Poteza;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.print("Othello je zabavno delo :) \n\n");
+
+		Igra igra = new Igra();
+		System.out.print(igra.naPotezi().toString());
+		System.out.print("\n\n");
+		polje_izpis(igra.getPlosca());
+		System.out.print("\n");
+		poteze_izpis(igra.poteze());
+		
+	}
+	
+	public static void polje_izpis(Polje[][] polje) {
+		for (Polje[] polja_i : polje) {
+			for (Polje polja_j : polja_i) {
+				if (polja_j == Polje.CRNO) {
+					System.out.print("B");
+				}
+				else if (polja_j == Polje.BELO) {
+					System.out.print("W");
+				}
+				else {
+					System.out.print("O");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	public static void poteze_izpis(Map<Poteza, LinkedList<Poteza>> moznosti) {
+		Set<Poteza> set_poteze = moznosti.keySet();
+		LinkedList<Poteza> poteze = new LinkedList<Poteza>();
+		poteze.addAll(set_poteze);
+		for (Poteza poteza : poteze) {
+			System.out.print(poteza.toString() + "\n");
+		}
+	}
+}
--- Othello/src/splosno/KdoIgra.java
+++ Othello/src/splosno/KdoIgra.java
@@ -1,12 +1,13 @@
 package splosno;
 
 public class KdoIgra {
-	// ime igralca
+
 	private String ime;
 	
 	public KdoIgra(String ime) {
 		this.ime = ime;
 	}
+	
 	@Override
 	public String toString() {
 		return ime;
