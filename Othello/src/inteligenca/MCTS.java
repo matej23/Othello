e7753c9327175a3cdd165dfe57a74a661ca3afd5
Tina Drnovsek <tina.drnovsek.si@gmail.com>
2022-05-21 11:55:53 +0200

    delujoc najdiNajboljsegaOtroka (upam)

--- Othello/src/inteligenca/MCTS.java
+++ Othello/src/inteligenca/MCTS.java
@@ -18,60 +18,45 @@ public class MCTS {
 	private static Random random = new Random ();
 	private static int cas = 2400;
 	
-//	public LinkedList<Poteza> moznePoteze;
-
 	public Igralec naPotezi;
 	
 	public MCTS(Igra igra) {
 		
 		naPotezi = igra.naPotezi();
 		drevo = new HashMap<TreeIndex, TreeEntry>();
-//		moznePoteze = igra.moznePoteze();
 
 	}
 	
-	public static Poteza nakljucnaPoteza(LinkedList<Poteza> poteze) {
-		int dolzina = poteze.size();
-		assert (dolzina > 0);
-		return poteze.getFirst();
-	}
 //	public static Poteza nakljucnaPoteza(LinkedList<Poteza> poteze) {
-//		assert (poteze.size() > 0);
-//		int randomIndex = random.nextInt(poteze.size());
-//		return poteze.get(randomIndex); 
+//		int dolzina = poteze.size();
+//		assert (dolzina > 0);
+//		return poteze.getFirst();
 //	}
-	public static void otrociPregled(Igra igra) {
-		for (Poteza poteza : igra.moznePoteze()) {
-			simulacija(igra, poteza);
+	public static Poteza nakljucnaPoteza(LinkedList<Poteza> poteze) {
+		assert (poteze.size() > 0);
+		int randomIndex = random.nextInt(poteze.size());
+		return poteze.get(randomIndex); 
+	}
+//	public static void otrociPregled(Igra igra) {
+//		for (Poteza poteza : igra.moznePoteze()) {
+//			simulacija(igra, poteza);
+//		}
+//	}
+	public static void otrociPregled(Igra igra, TreeIndex i) {
+		LinkedList<Poteza> klon = (LinkedList<Poteza>) igra.moznePoteze().clone();
+		
+		while (!klon.isEmpty()) {
+			Poteza nakljucnaPoteza = nakljucnaPoteza(klon);
+			simulacija(igra, nakljucnaPoteza, i);
+			klon.remove(nakljucnaPoteza);
 		}
 	}
-//	public static void otrociPregled(Igra igra) {
-//		LinkedList<Poteza> klon = (LinkedList<Poteza>) igra.moznePoteze().clone();
-//		LinkedList<Poteza> uporabljene = new LinkedList<Poteza>();
-//		
-//		while (uporabljene.size() < igra.moznePoteze().size()) {
-//			Poteza nakljucnaPoteza = nakljucnaPoteza(klon);
-//			simulacija(igra, nakljucnaPoteza);
-//			klon.remove(nakljucnaPoteza);
-//			uporabljene.add(nakljucnaPoteza);
-//		}
-//	}
 	
-//	public static void otrociPregled(Igra igra) {
-//		while (!igra.moznePoteze().isEmpty()) {
-//			Poteza nakljucnaPoteza = nakljucnaPoteza(igra);
-//			simulacija(igra, nakljucnaPoteza);
-//			igra.moznePoteze().remove(nakljucnaPoteza);
-//		}
-//		
-//	}
 	
-	// UPAM, DA JE TO PRAV
-	
-	public static void simulacija(Igra igra, Poteza poteza) {
+	public static void simulacija(Igra igra, Poteza poteza, TreeIndex i) {
 		Igra kopijaIgre = new Igra(igra.plosca, igra.naPotezi(), igra.stanje);
 		Igralec igralec = kopijaIgre.naPotezi();
-		TreeIndex indeks = new TreeIndex(new TreeIndex(), poteza);
+		TreeIndex indeks = new TreeIndex(i, poteza);
 		while (kopijaIgre.stanje == Stanje.V_TEKU) {
 			if (kopijaIgre.moznePoteze().size() > 0) {
 				Poteza nakljucna = nakljucnaPoteza(kopijaIgre.moznePoteze());
@@ -92,35 +77,40 @@ public class MCTS {
 		else if (stanje == Stanje.NEODLOCENO) return 0.5;
 		else return 0;
 	}
+
+	
 	public static TreeIndex najdiNajboljsegaOtroka(Igra igra) {
 		TreeIndex maksimalen = new TreeIndex();
+		int j = 0;
 		for (TreeIndex i : drevo.keySet()) {
-			maksimalen = i;
+			if (j > 0) {
+				if (vrednostUCT(i, drevo.get(i)) > vrednostUCT(maksimalen, drevo.get(maksimalen))) {
+					maksimalen = i;
+				}
+			}
+			// samo na zacetku, ko je j = 0, vzamemo za maksimalen prvi kljuc v drevesu
+			else {
+				maksimalen = i;
+				j++;
+			}
 		}
 		return maksimalen;
-	
 	}
-//	public static TreeIndex najdiNajboljsegaOtroka(Igra igra) {
-//		TreeIndex maksimalen = new TreeIndex();
-//		for (TreeIndex i : drevo.keySet()) {
-//			if (vrednostUCT(i, drevo.get(i)) > (drevo.get(maksimalen).zmage / drevo.get(maksimalen).poskusi)) {
-//				maksimalen = i;
-//			}
-//		}
-//		return maksimalen;
-//	}
 	
 	public static double vrednostUCT (TreeIndex i,TreeEntry e) {
+		if (drevo.containsKey(i.parent())) {
 			return e.zmage / e.poskusi + Math.sqrt(2 * Math.log(drevo.get(i.parent()).poskusi) / e.poskusi);
 		}
+		else return e.zmage / e.poskusi;
+	}
 	
 	public Poteza main(Igra igra) {
 		long zacetek = System.currentTimeMillis();
 		Igra kopijaIgre = new Igra(igra.plosca, igra.naPotezi(), igra.stanje);
-		TreeIndex otrok = null;
+		TreeIndex otrok = new TreeIndex();
 		while (zacetek + cas > System.currentTimeMillis()) {
 			if (!kopijaIgre.poteze().isEmpty()) {
-				otrociPregled(kopijaIgre);
+				otrociPregled(kopijaIgre, otrok);
 				otrok = najdiNajboljsegaOtroka(kopijaIgre);
 				Igra kopija = drevo.get(otrok).igra;
 				kopijaIgre = new Igra(kopija.plosca, kopija.naPotezi(), kopija.stanje);
